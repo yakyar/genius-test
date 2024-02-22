f063963e72cc067ba98155880cf45025fe2c1fc0
yakyar <155535426+yakyar@users.noreply.github.com>
2024-02-22 23:07:50 +0200

    22--02

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -11,6 +11,7 @@ menuBtnClose.addEventListener("click", toggleMenu);
 
 menuBtnOpen.addEventListener("click", disableScroll);
 menuBtnClose.addEventListener("click", disableScroll);
+
 document.querySelectorAll(".mobile-menu__link").forEach((a) => {
   a.addEventListener("click", () => {
     document.querySelector(".mobile-menu").className = "mobile-menu";
